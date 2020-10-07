0ad4dde52ebc6d1842f0d6e756ec2eb9ae465899
Aaron Lauson <alauson@gmail.com>
2020-10-07 14:41:26 -0500

    win condition logic completed

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-console.log('script is working')
+const winningCombosIndex = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+const valArr = document.querySelectorAll('.column')
 
-const valArr = document.querySelector('.container')
-let gridVal = (valArr.textContent).replace(/\W+/g, '')
+let whosTurn = 'O'
+let tempStr = ""
 
-console.log(gridVal)
+// get value of each square and convert to a string
+// 1 if it matches the player whose turn it was
+// 0 if it belongs to the non-turn player or is null
+valArr.forEach(char => {
+    console.log(char.textContent)
+    if (char.textContent === whosTurn){
+        tempStr = tempStr+"1"
+    } else {
+        tempStr = tempStr+'0'
+    }
+})
+
+// determine the index of all the players squares and put into an array
+let indexMatchArr = []
+let regexp = /1/g;
+let matches = [...tempStr.matchAll(regexp)];//todo; understand the ellipsis in this statement
+matches.forEach((match) => {
+    indexMatchArr.push(match.index)
+});
+
+// if the index array has one of the winning combinations, declare the winner
+winningCombosIndex.forEach(combo => {
+    let intersecton = combo.filter(x => indexMatchArr.includes(x))
+    if(intersecton.length === 3){
+        console.log("We have a winner")
+        console.log(combo)
+    }
+})
