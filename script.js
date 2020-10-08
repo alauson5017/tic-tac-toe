7939ad43ee002aba59bcf4fb5812c327a087fea6
Aaron Lauson <alauson@gmail.com>
2020-10-08 14:04:23 -0500

    completed turn logic

--- script.js
+++ script.js
@@ -2,37 +2,35 @@ const winningCombosIndex = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4
 const valArr = document.querySelectorAll('.column')
 const newGameButton = document.querySelector('#newGameButton')
 
-let whosTurn = ''
-let whosTurnButton = document.querySelector("#whosTurn")//.textContent
+let whosTurn = undefined
+let isDraw = false
 let winner = false
-console.log('whosTurnButton.textContent', whosTurnButton.textContent)
-
-
-
-
+let whosTurnButton = document.querySelector("#whosTurn")
+let pickButtons = document.querySelectorAll("#pick")
 
 
 // get value of each square and convert to a string
 // 1 if it matches the player whose turn it was
 // 0 if it belongs to the non-turn player or is null
 function changeTurn() {
-    if (whosTurn != '') {
+    if (whosTurn != undefined) {
         if (whosTurn === 'X'){
             whosTurn = 'O'
         } else if (whosTurn === 'O'){
             whosTurn = 'X'
-        } else {
-            addMessage(`X will start by default`)
-            whosTurn = 'X'
         }
         whosTurnButton.textContent = `Player ${whosTurn}'s Turn`
     } else {
-        console.log('unknown turn')
-        whosTurn = 'O'
+        whosTurnButton.style.display = "none";
+        pickButtons.forEach(button => {
+            button.style.display = "inline";
+        })
+            
     }
 }
 function getBoardValues(player) {
     let tempStr = ""
+
     valArr.forEach(char => {
         console.log(char.textContent)
         if (char.textContent === whosTurn){
@@ -49,6 +47,8 @@ function getBoardValues(player) {
     matches.forEach((match) => {
         indexMatchArr.push(match.index)
     });
+    console.log('indexMatchArr', indexMatchArr)
+
     return indexMatchArr
 }
 
@@ -69,55 +69,76 @@ function addMessage(message) {
     const template = document.querySelector('#message-template');
     const messageUL = document.querySelector('#message-ul');
     let messageBlock = document.importNode(template.content, true);
-    if (winner){
-        messageBlock.querySelector('h1').textContent = whosTurn+ " is the winner!"
+    if (winner || isDraw){
+        while (messageUL.childElementCount > 0) {
+            document.querySelector('#message-ul').lastElementChild.remove()
+        }
+        if (isDraw) {
+            messageBlock.querySelector('h1').textContent = "You have battled to a draw"
+        } else {
+            messageBlock.querySelector('h1').textContent = whosTurn+ " is the winner!"
+        }
         messageBlock.querySelector('blockquote').textContent = ""
     } else {
         messageBlock.querySelector('blockquote').textContent = "> "+message
     }
     messageBlock.querySelector('p').textContent = ""
     messageUL.insertBefore(messageBlock, messageUL.children[0])
-    console.log('messageUL.childElementCount', messageUL.childElementCount)
+    // console.log('messageUL.childElementCount', messageUL.childElementCount)
     if (messageUL.childElementCount > 3){
-        console.log('time to remove a child')
         document.querySelector('#message-ul').lastElementChild.remove()
     }
 }
 
 newGameButton.addEventListener('click', (e) => {
     winner = false
-    whosTurn = ''
+    whosTurn = undefined
     valArr.forEach(char => {
         char.textContent = ""
     })
     addMessage("Starting new game")
+    changeTurn()
 })
 
 const squaresContainer = document.querySelector('.container')
 squaresContainer.addEventListener('click', (e) => {
-    if (whosTurn === '') {
-        changeTurn()
-    }
+    console.log(e.target.id)
+    console.log('whosTurn', whosTurn)
+    if (whosTurn == undefined && e.target.id == 'pick') {
+        console.log(e.target.textContent[0])
+        whosTurn = e.target.textContent[0]
+        whosTurnButton.style.display = "inline";
+        pickButtons.forEach(button => {
+            button.style.display = "none";
+
+    })
+    whosTurnButton.textContent = `Player ${whosTurn}'s Turn`
+
+}
     if (!winner){
         if (e.target.classList.contains('gameSquare') && e.target.textContent === "") {
             e.target.textContent = whosTurn
             addMessage(`${whosTurn} turn is over`)
             let boardVals = getBoardValues(whosTurn)
             console.log('boardVals',boardVals)
-            hasWon(boardVals)
-            console.log('hasWon(boardVals)', winner)
-            if (winner) {
-                addMessage(`${whosTurn} is the winner!`)
+            if (boardVals.length < 5) {
+                hasWon(boardVals)
+                console.log('hasWon(boardVals)', winner)
+                if (winner) {
+                    addMessage(`${whosTurn} is the winner!`)
+                } else {
+                    changeTurn()
+                    console.log('whosTurn', whosTurn)
+                }
             } else {
-                changeTurn()
-                console.log('whosTurn', whosTurn)
+                isDraw = true
+                addMessage("match is ended in a draw")
+                
             }
         }
-
-    } else {
 
     }
 
 })
-
+addMessage("welcome to game")
 whosTurnButton.addEventListener('click', changeTurn())
